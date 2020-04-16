04f1f0db7a0abad21c55ec251d4cb228b52a11ee
Pacifique78 <tuyipaccy78@gmail.com>
2020-04-16 15:17:19 +0200

    consume backend

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization ,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
 app.get('/', (req, res) => res.status(200).json({ message: 'WELCOME TO SHOP_WITH_US' }));
 app.use(authRouter);
 app.use(orderRouter);
---
+++ web/js/connect.js
@@ -0,0 +1,85 @@
+document.getElementById('register').addEventListener('submit', async (e) => {
+  e.preventDefault();
+  [...document.getElementsByClassName('fields')].forEach((field) => {
+    // eslint-disable-next-line no-param-reassign
+    field.style.display = 'none';
+  });
+  document.getElementById('error').style.display = 'none';
+  const name = document.getElementById('name').value;
+  const userName = document.getElementById('userName').value;
+  const phoneNumber = document.getElementById('phoneNumber').value;
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('confirmPassword').value;
+  const selected = document.getElementById('cont-typ');
+  const isBuyer = selected.options[selected.selectedIndex].value;
+
+  const response = await fetch('http://localhost:4500/auth/signup', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      name, userName, phoneNumber, password, confirmPassword, isBuyer,
+    }),
+  });
+  const json = await response.json();
+  if (json.error) {
+    if (json.status === 409) {
+      const errorDiv = document.getElementById('error');
+      errorDiv.innerHTML = json.error;
+      errorDiv.style.display = 'inline';
+      errorDiv.style.color = '#d84930';
+    } else {
+      const errorDiv = document.getElementById(`${json.path}-div`);
+      errorDiv.innerHTML = json.error;
+      errorDiv.style.display = 'inline';
+      errorDiv.style.color = '#d84930';
+    }
+  } else {
+    sessionStorage.setItem('Authorization', `${json.token}`);
+    document.getElementById('login-btn-redirect').click();
+  }
+});
+
+
+document.getElementById('login').addEventListener('submit', async (e) => {
+  e.preventDefault();
+  [...document.getElementsByClassName('fields')].forEach((field) => {
+    // eslint-disable-next-line no-param-reassign
+    field.style.display = 'none';
+  });
+  document.getElementById('login-error').style.display = 'none';
+  const userName = document.getElementById('login-userName').value;
+  const password = document.getElementById('login-password').value;
+
+  const response = await fetch('http://localhost:4500/auth/signin', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      userName, password,
+    }),
+  });
+  const json = await response.json();
+
+  console.log(json);
+  if (json.error) {
+    if (json.status === 401) {
+      const errorDiv = document.getElementById('login-error');
+      errorDiv.innerHTML = json.error;
+      errorDiv.style.display = 'inline';
+      errorDiv.style.color = '#d84930';
+    } else {
+      const errorDiv = document.getElementById(`login-${json.path}-div`);
+      errorDiv.innerHTML = json.error;
+      errorDiv.style.display = 'inline';
+      errorDiv.style.color = '#d84930';
+    }
+  } else {
+    sessionStorage.setItem('Authorization', `${json.data.token}`);
+    document.getElementById('login').submit();
+  }
+});
